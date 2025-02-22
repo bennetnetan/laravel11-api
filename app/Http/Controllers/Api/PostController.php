8cfa3dfdd3619d8a868fb7dc023b03effef02564
Benja Netan <netanyahubenjamin70@gmail.com>
2025-02-22 13:22:13 +0300

    Add show, update, and destroy methods to PostController for post management

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PostResource;
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -50,5 +51,75 @@ class PostController extends Controller
 
         //return response
         return new PostResource(true, 'Data post created successfully!', $post);
+    }
+
+    public function show($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+        public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/' . basename($post->image));
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data post updated successfully!', $post);
+    }
+
+        public function destroy($id)
+    {
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete image
+        Storage::delete('public/posts/'.basename($post->image));
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data post deleted successfully!', null);
     }
 }
